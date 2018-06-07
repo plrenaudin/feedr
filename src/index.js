347c0aec517d5f5db4995676ad7d2fcfc254ad84
plrenaudin <pl.renaudin@gmail.com>
2018-06-06 22:41:49 -0400

    add sw and manifest

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import { render } from "inferno";
 import App from "./App";
 import { Provider } from "inferno-mobx";
 import Store from "./modules/store";
+
 const store = new Store();
+
 render(
   <Provider store={store}>
     <App />
