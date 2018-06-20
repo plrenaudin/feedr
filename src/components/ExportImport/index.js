28ddb31c87ae4b018227751b145ff7c50ba2a7b9
plrenaudin <pl.renaudin@gmail.com>
2018-06-19 20:43:56 -0400

    improve export import component

--- src/components/ExportImport/index.js
+++ src/components/ExportImport/index.js
@@ -1,64 +1,49 @@
-import { linkEvent, Component } from "inferno";
 import db from "../../modules/db";
 import { shortFormatDate } from "../../modules/formatter";
 import Icon from "../Icon";
 
-class ExportImport extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { displayInput: false };
-  }
+const exportData = () => {
+  db
+    .getAll()
+    .then(data => {
+      const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(data));
+      const downloadAnchorNode = document.createElement("a");
+      downloadAnchorNode.setAttribute("href", dataStr);
+      downloadAnchorNode.setAttribute("download", `feedrExport-${shortFormatDate()}.json`);
+      document.body.appendChild(downloadAnchorNode); // required for firefox
+      downloadAnchorNode.click();
+      downloadAnchorNode.remove();
+    })
+    .catch(e => console.error(e));
+};
 
-  exportData() {
-    db
-      .getAll()
-      .then(data => {
-        const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(data));
-        const downloadAnchorNode = document.createElement("a");
-        downloadAnchorNode.setAttribute("href", dataStr);
-        downloadAnchorNode.setAttribute("download", `feedrExport-${shortFormatDate()}.json`);
-        document.body.appendChild(downloadAnchorNode); // required for firefox
-        downloadAnchorNode.click();
-        downloadAnchorNode.remove();
-      })
-      .catch(e => console.error(e));
-  }
+const onFileSelected = e => {
+  const reader = new FileReader();
+  reader.onload = () => importData(reader.result);
+  reader.readAsText(e.target.files[0]);
+};
 
-  toggleInput(instance) {
-    instance.setState({ displayInput: !instance.state.displayInput });
+const importData = data => {
+  try {
+    const parsed = JSON.parse(data);
+    db.import(parsed).catch(e => console.error(e));
+  } catch (e) {
+    console.error(e);
   }
+};
 
-  onFileSelected(instance, e) {
-    const reader = new FileReader();
-    reader.onload = () => instance.importData(reader.result);
-    reader.readAsText(e.target.files[0]);
-  }
-
-  importData(data) {
-    try {
-      const parsed = JSON.parse(data);
-      db.import(parsed).catch(e => console.error(e));
-    } catch (e) {
-      console.error(e);
-    }
-  }
-
-  render() {
-    return (
-      <div class="export-import">
-        <div class="export" onClick={linkEvent(this, this.exportData)}>
-          <Icon name="download3" />
-        </div>
-        <div class="import" onClick={linkEvent(this, this.toggleInput)}>
-          <Icon name="upload3" />
-
-          {this.state.displayInput && (
-            <input type="file" onChange={linkEvent(this, this.onFileSelected)} />
-          )}
-        </div>
-      </div>
-    );
-  }
-}
+const ExportImport = () => (
+  <div class="export-import">
+    <div class="export" onClick={exportData}>
+      <Icon name="download3" />
+    </div>
+    <div class="import">
+      <label>
+        <Icon name="upload3" />
+        <input type="file" onChange={onFileSelected} />
+      </label>
+    </div>
+  </div>
+);
 
 export default ExportImport;
