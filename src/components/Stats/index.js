087305d9f5725730eb80684be21403f1871de635
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-06-28 00:32:09 +0000

    Deploy plrenaudin/feedr to github.com/plrenaudin/feedr.git:gh-pages

--- src/components/Stats/index.js
+++ src/components/Stats/index.js
@@ -1,26 +1,48 @@
 import { inject } from "inferno-mobx";
 import { Component } from "inferno";
 import Pie from "../Pie";
+import { DAY, MONTH, WEEK } from "../../modules/dateRangeEnum";
+
+const defaultState = () => ({ data: [], selected: DAY });
 
 class Stats extends Component {
   constructor(props) {
     super(props);
     this.store = props.store;
-    this.state = { data: [] };
+    this.state = defaultState();
   }
-  componentDidMount() {
-    this.store.fetchStats().then(data =>
+
+  fetchStats = range => () => {
+    this.store.fetchStats(range).then(data =>
       this.setState({
-        data
+        data,
+        selected: range
       })
     );
+  };
+
+  componentDidMount() {
+    this.fetchStats(DAY)();
   }
+
   componentWillUnmount() {
-    this.setState({ data: [] });
+    this.setState(defaultState());
   }
+
   render() {
     return (
       <section class="stats">
+        <ul class="control">
+          <li class={this.state.selected === DAY && "selected"} onClick={this.fetchStats(DAY)}>
+            Day
+          </li>
+          <li class={this.state.selected === WEEK && "selected"} onClick={this.fetchStats(WEEK)}>
+            Week
+          </li>
+          <li class={this.state.selected === MONTH && "selected"} onClick={this.fetchStats(MONTH)}>
+            Month
+          </li>
+        </ul>
         <Pie categories={this.state.data} />
       </section>
     );
